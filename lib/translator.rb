8deb6a5a90ddadb992af758c7e7c02e2a1606858
Sam Gold <sgold5757@gmail.com>
2020-10-22 14:53:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   emoticon_meaning = library.find {|definition, hash| emoticon == definition[:english]}
+  binding.pry
   emoticon_meaning[:japanese]
 end
 
