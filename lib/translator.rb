716bfc1f1162bb1404b1945eff30889de35c584d
Sam Gold <sgold5757@gmail.com>
2020-10-22 15:19:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,14 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  desired_emo = library.find{|definition, hash| emoticon == library[definition][:japanese]}
+  if !desired_emo
+    return "Sorry, that emoticon was not found"
+  else
+    desired_emo[0]
+  end
 end
 
 #bilingual_dict = {}
